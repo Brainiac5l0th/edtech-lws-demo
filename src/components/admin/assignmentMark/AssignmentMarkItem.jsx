9de16f262341984b50e29ab3dfdfc77fcf008d32
Shawon Talukder <shawontalukder.study@gmail.com>
2023-04-01 23:34:48 +0600

    bug fix, user now can not submit more than total mark

--- src/components/admin/assignmentMark/AssignmentMarkItem.jsx
+++ src/components/admin/assignmentMark/AssignmentMarkItem.jsx
@@ -1,26 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useUpdateAssignmentMarkMutation } from '../../../features/markAssignment/markAssignmentApi';
-import { formateDate, formatString } from "../../../utils/formatting";
+import { formatString, formateDate } from "../../../utils/formatting";
 
 const AssignmentMarkItem = ({ markInfo }) => {
     const { id, title, repo_link, createdAt, status, student_name, mark, totalMark } = markInfo || {};
     const [assignmentMark, setAssignmentMark] = useState(mark);
     const [updateAssignmentMark, { isLoading }] = useUpdateAssignmentMarkMutation();
-
+    const [error, setError] = useState(false);
     const formatedTitle = formatString(title);
     const formatedDate = formateDate(createdAt, true);
+    const inputRef = useRef(null);
 
+    //effects
+    useEffect(() => {
+        if (error) {
+            setAssignmentMark("");
+            inputRef.current.focus();
+            setError(false);
+        }
+    }, [error])
     //handlers
     const handleChange = (e) => {
         setAssignmentMark(e.target.value)
     }
-    const handleUpdateMark = (e) => {
-        const data = {
-            status: "published",
-            mark: Number(assignmentMark),
+    const handleUpdateMark = () => {
+        if (assignmentMark > totalMark) {
+            setError(true);
+        } else {
+            const data = {
+                status: "published",
+                mark: Number(assignmentMark),
+            }
+            updateAssignmentMark({ id, data })
         }
-        updateAssignmentMark({ id, data })
-
     }
     //class modify
     const markClass = status === "pending" ? "input-mark" : ""
@@ -28,7 +40,7 @@ const AssignmentMarkItem = ({ markInfo }) => {
     //content modify
     const markContent = status === "pending" ?
         <>
-            <input max={totalMark} value={assignmentMark} onChange={handleChange} />
+            <input max={totalMark} ref={inputRef} value={assignmentMark} onChange={handleChange} />
             <button disabled={isLoading} onClick={handleUpdateMark}>
                 <svg fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor"
                     className="w-6 h-6 text-green-500 cursor-pointer hover:text-green-400">
