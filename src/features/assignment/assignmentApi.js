4ba4671bc0e86b6bb87c73c4d5d599b864ab344d
Shawon Talukder <shawontalukder.study@gmail.com>
2023-04-07 21:41:30 +0600

    changes

--- src/features/assignment/assignmentApi.js
+++ src/features/assignment/assignmentApi.js
@@ -42,6 +42,47 @@ const assignmentApi = apiSlice.injectEndpoints({
         } catch (error) {}
       },
     }),
+    updateAssignmentVideoTitle: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `assignments/${id}`,
+        method: "PATCH",
+        body: data,
+      }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          const result = await queryFulfilled;
+          const { data, meta } = result;
+          if (data?.id && meta?.response?.status === 200) {
+            //update assignments cache data
+            dispatch(
+              apiSlice.util.updateQueryData(
+                "getAssignments",
+                undefined,
+                (draft) => {
+                  const editableDraft = draft.find(
+                    (assignment) => Number(assignment.id) === Number(arg.id)
+                  );
+                  editableDraft.video_title = data.video_title;
+                }
+              )
+            );
+            //update cache of that assignment
+            dispatch(
+              apiSlice.util.updateQueryData(
+                "getAssignment",
+                arg.id,
+                (draft) => {
+                  const editableDraft = draft.find(
+                    (assignment) => Number(assignment.id) === Number(arg.id)
+                  );
+                  editableDraft.video_title = data.video_title;
+                }
+              )
+            );
+          }
+        } catch (error) {}
+      },
+    }),
     updateAssignment: builder.mutation({
       query: ({ id, data }) => ({
         url: `assignments/${id}`,
@@ -78,6 +119,38 @@ const assignmentApi = apiSlice.injectEndpoints({
                 draft.totalMark = data.totalMark;
               })
             );
+            const dataToPass = {
+              title: data.title,
+              totalMark: data.totalMark,
+            };
+
+            //also update all assignment marks
+            const assignmentMarks = await dispatch(
+              apiSlice.endpoints.getAssignmentMarkByAssignmentId.initiate(id)
+            ).unwrap();
+            //if there is a change to total Mark, then delete assignment mark
+            if (
+              assignmentMarks?.length > 0 &&
+              assignmentMarks[0]?.totalMark !== data.totalMark
+            ) {
+              //delete the assignment mark
+              assignmentMarks.forEach((assignmentMark) => {
+                dispatch(
+                  apiSlice.endpoints.deleteAssignmentMark.initiate(
+                    assignmentMark.id
+                  )
+                );
+              });
+            } else {
+              assignmentMarks.forEach((assignmentMark) => {
+                dispatch(
+                  apiSlice.endpoints.updateAssignmentMarkTitle.initiate({
+                    id: assignmentMark.id,
+                    data: dataToPass,
+                  })
+                );
+              });
+            }
           }
         } catch (error) {}
       },
@@ -103,6 +176,21 @@ const assignmentApi = apiSlice.injectEndpoints({
                 }
               )
             );
+
+            //delete all Assignment against this assignment id
+            const assignmentMarks = await dispatch(
+              apiSlice.endpoints.getAssignmentMarkByAssignmentId.initiate(arg)
+            ).unwrap();
+
+            if (assignmentMarks?.length > 0) {
+              assignmentMarks.forEach((markAssignment) => {
+                dispatch(
+                  apiSlice.endpoints.deleteAssignmentMark.initiate(
+                    markAssignment.id
+                  )
+                );
+              });
+            }
           }
         } catch (error) {}
       },
